045df2d867ad37f6fb5c138bcb39cca4ce0a5858
Estefania <davilaestefania17@gmail.com>
2024-08-23 19:01:25 -0500

    agregando archivos

---
+++ js/cifrado.js
@@ -0,0 +1,36 @@
+function descifrado_cifrado(texto, desplazamiento, accion) {
+    const abecedario = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let resultado = '';
+    desplazamiento = desplazamiento % abecedario.length;
+    
+    for (let i = 0; i < texto.length; i++) {
+        const letraActual = texto[i].toUpperCase();
+        const indiceActual = abecedario.indexOf(letraActual);
+
+        if (indiceActual === -1) {
+            resultado += letraActual;
+        } else {
+            let nuevoIndice;
+            if (accion == 1) {
+                // Cifrado
+                nuevoIndice = (indiceActual + desplazamiento) % abecedario.length;
+            } else {
+                // Descifrado
+                nuevoIndice = (indiceActual - desplazamiento) % abecedario.length;
+                if (nuevoIndice < 0) {
+                    nuevoIndice += abecedario.length;
+                }
+            }
+            resultado += abecedario[nuevoIndice];
+        }
+    }
+    return resultado;
+}
+
+/*
+let textoOriginal = "ZGDS";
+let desplazamiento = 18;
+let textoCifrado = descifrado_cifrado(textoOriginal,desplazamiento,0);
+console.log(textoCifrado);*/
+
+export default descifrado_cifrado;
